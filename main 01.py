df930e9f28840183a48671ebe2f350bc3a71d75a
Lorena Nascimento <ceuazul222@icloud.com>
2025-08-05 21:48:46 -0300

    Update main 01.py

--- main 01.py
+++ main 01.py
@@ -6,8 +6,8 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 with open("config.json", "r") as f:
     config = json.load(f)
 
-TOKEN = config["telegram_token"]
-CHANNEL_LINK = config["channel_link"]
+TOKEN = config["8317307798:AAFhi32HH2RjoRPRQsGoKDDAl8BhrOwMpy4"]
+CHANNEL_LINK = config["https://t.me/+AndvE0ZgfIxiZWRh"]
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Olá! Enviarei a cobrança Pix em instantes.")
